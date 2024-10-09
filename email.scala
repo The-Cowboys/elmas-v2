76e84938a5e67841bc116d67e2200932de6f9872
Franco Sangiacomo <franconecat@gmail.com>
2024-10-09 10:35:13 +0200

    Format email script

--- email.scala
+++ email.scala
@@ -1,39 +1,39 @@
-import cats.effect.{IO, Resource}
-import org.http4s.client._
-import org.http4s.ember.client._
-import org.http4s._
-import org.http4s.implicits._
-import org.http4s.circe._
+import cats.effect.{ IO, Resource }
 import io.circe.Json
-import io.circe.syntax._
+import io.circe.syntax.*
+import org.http4s.*
+import org.http4s.circe.*
+import org.http4s.client.*
 import org.http4s.client.middleware.Logger
+import org.http4s.ember.client.*
+import org.http4s.implicits.*
 
 final class EmailClient(internal: Client[IO], token: String) {
 
-  val url = uri"https://api.resend.com/emails"
+    val url = uri"https://api.resend.com/emails"
 
-  val client = Logger[IO](
-    logHeaders = false,
-    logBody = true,
-    logAction = Some((msg: String) => IO.println(msg))
-  )(internal)
+    val client = Logger[IO](
+        logHeaders = false,
+        logBody = true,
+        logAction = Some((msg: String) => IO.println(msg))
+    )(internal)
 
-  def sendEmail(cowboys: List[Cowboy], tonto: Cowboy): IO[Unit] =
-    val receivers = cowboys.map(_.email)
-    val emailPayload = Json.obj(
-      "from"    -> "The Cowboys <tonto@thecowboys.one>".asJson,
-      "to"      -> receivers.asJson,
-      "subject" -> "Hello World".asJson,
-      "html"    -> "<p>Congrats on sending your <strong>first email</strong>!</p>".asJson
-    )
-
-    IO.println("Sending emails") *> client.expect[Unit](
-      Request[IO](Method.POST, url)
-        .withEntity(emailPayload)
-        .withHeaders(
-          Header("Content-Type", "application/json"),
-          Header("Authorization", s"Bearer $token")
+    def sendEmail(cowboys: List[Cowboy], tonto: Cowboy): IO[Unit] =
+        val receivers = cowboys.map(_.email)
+        val emailPayload = Json.obj(
+            "from"    -> "The Cowboys <tonto@thecowboys.one>".asJson,
+            "to"      -> receivers.asJson,
+            "subject" -> "Hello World".asJson,
+            "html"    -> "<p>Congrats on sending your <strong>first email</strong>!</p>".asJson
         )
-    ) <* IO.println("Sent email to cowboys")
+
+        IO.println("Sending emails") *> client.expect[Unit](
+            Request[IO](Method.POST, url)
+                .withEntity(emailPayload)
+                .withHeaders(
+                    Header("Content-Type", "application/json"),
+                    Header("Authorization", s"Bearer $token")
+                )
+        ) <* IO.println("Sent email to cowboys")
 
 }
